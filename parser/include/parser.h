d5dade49f1e589130147794920dafbc59d34ed53
Tim Herreijgers <timtim3001@gmail.com>
2023-08-23 18:16:16 +0200

    Added empty parser with empty tests

---
+++ parser/include/parser.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright © 2022 Tim Herreijgers
+ * Licensed using the MIT license
+ */
+
+#pragma once
+
+namespace TimC::Parser
+{
+
+} // namespace TimC::Lexer
---
+++ tests/parser_tests/parser_tests.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright © 2022 Tim Herreijgers
+ * Licensed using the MIT license
+ */
+
+#include "parser.h"
+
+#include "gtest/gtest.h"
+#include "gtest/gtest-matchers.h"
+#include "gmock/gmock-matchers.h"
+
+namespace TimC::Lexer
+{
+
+class ParserTests : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+
+    }
+};
+
+} // namespace TimC::Lexer
